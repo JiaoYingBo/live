05f6b1ddf62c4928eaf24f8fd69946f7bc7f6300
JiaoYingBo <715775799@qq.com>
2017-09-26 20:10:14 +0800

    test

--- 直播学习/Classes/Home/View/HomeViewCell.swift
+++ 直播学习/Classes/Home/View/HomeViewCell.swift
@@ -11,18 +11,20 @@ import Kingfisher
 
 class HomeViewCell: UICollectionViewCell {
     
-    // MARK: 控件属性
+    // 控件属性
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var liveImageView: UIImageView!
     @IBOutlet weak var nickNameLabel: UILabel!
     @IBOutlet weak var onlinePeopleLabel: UIButton!
     
-    // MARK: 定义属性
+    // 定义属性
     var anchorModel : AnchorModel? {
         didSet {
             albumImageView.setImage(anchorModel!.isEvenIndex ? anchorModel?.pic74 : anchorModel?.pic51, "home_pic_default")
+            
             liveImageView.isHidden = anchorModel?.live == 0
             nickNameLabel.text = anchorModel?.name
+            
             onlinePeopleLabel.setTitle("\(anchorModel?.focus ?? 0)", for: .normal)
         }
     }
